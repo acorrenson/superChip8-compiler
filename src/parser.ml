53f220ca0bf8aa8efa1f191414611f9b437f0532
Arthur Correnson <arthur.correnson@gmail.com>
2019-04-13 15:16:51 +0200

    Rewrite the lexer properly

--- src/parser.ml
+++ src/parser.ml
@@ -1,7 +1,7 @@
 (* --------------------------------------- *)
 (* - Parser for chip-8 assembly language - *)
-(* - Written by A. Correnson ------------- *) 
-(* - and T. Barriere  -------------------- *)
+(* - Written by A. Correnson             - *) 
+(* - and T. Barriere                     - *)
 (* --------------------------------------- *)
 
 open Str
@@ -55,102 +55,3 @@ let main =
   try check_ins c (* should fail *)
   with Adress_out_of_range ->
     print_endline "JP 5000 is not a valid instruction"
-
-
-(* Alias for String.make *)
-let to_str s =
-  (String.make 1 s)
-
-(* Prepend char to string *)
-let prepend c s =
-  (String.make 1 c) ^ s
-
-(* Append char to string *)
-let append s c =
-  s ^ (String.make 1 c)
-
-
-let rev l =
-  let rec rev_r l lr =
-    match l with
-    | [] -> lr
-    | h::t -> rev_r t (h::lr)
-  in
-  rev_r l []
-
-let rec fill_buffer ic buff =
-  try
-    let c = input_char ic in
-    fill_buffer ic (c::buff)
-  with End_of_file -> rev buff
-
-type lexem = 
-  | Lins of string
-  | Ladr of int
-  | Lsep
-
-let pp_lexem lx =
-  match lx with
-  | Lins str -> print_endline str
-  | Ladr adr -> print_endline (string_of_int adr)
-  | Lsep -> print_endline ","
-
-(* test if a character is a digit *)
-let is_digit d =
-  let re = Str.regexp "[0-9]" in
-  Str.string_match re (to_str d) 0
-
-let is_char d =
-  let re = Str.regexp "[_A-Za-z]" in
-  Str.string_match re (to_str d) 0
-
-let is_sep d =
-  match d with
-  '\n' | ' ' -> true
-  | _ -> false
-
-(* Scan for a number *)
-let scan_number buff =
-  let rec _scan_number buff number =
-    match buff with
-    | d::nbuff when is_digit d ->
-      _scan_number nbuff (append number d)
-    | _ -> Ladr (int_of_string number), buff
-  in
-  _scan_number buff ""
-
-
-(* Scan for an instruction *)
-let scan_ins buff =
-  let rec _scan_string buff str =
-    match buff with
-    | c::nbuff when is_sep c -> Lins str, buff
-    | c::nbuff -> _scan_string nbuff (append str c)
-    | [] -> Lins str, buff
-  in
-  _scan_string buff ""
-
-(* lex | tokenize a file *)
-(* NOTE : This function is an exemple, it is not the real lexer *)
-let lex f =
-  let rec lex_r buff l =
-    match buff with
-    | c::tail when is_digit c ->
-      let lx, nbuff = scan_number buff in
-      lex_r nbuff (lx::l)
-    | c::tail when c == ',' ->
-      lex_r tail (Lsep::l)
-    | c::tail when is_sep c ->
-      lex_r tail l
-    | c::tail when is_char c ->
-      let lx, nbuff = scan_ins buff in
-      lex_r nbuff (lx::l)
-    | [] -> l
-    | _ -> failwith "Lexing error"
-  in
-  let ic = open_in f in
-  let buff = fill_buffer ic [] in
-  rev (lex_r buff [])
-
-let _ =
-  List.iter pp_lexem (lex "test.txt");
