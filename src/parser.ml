3e0be2e5135eeeb3c5a30ee56085bb61fbb35514
Arthur Correnson <arthur.correnson@gmail.com>
2019-04-17 15:06:51 +0200

    Debug some opcode bin

--- src/parser.ml
+++ src/parser.ml
@@ -44,10 +44,24 @@ type ins =
   | SKP of arg
   | SKNP of arg
   | JP of arg
+  | JP2 of arg * arg
   | DW of arg
   | END
   | LBL of string
 
+let pp_arg a =
+  match a with
+  | Reg a -> print_int a; print_newline ();
+  | Adr n -> print_int n; print_newline ();
+  | Cst n -> print_int n; print_newline ();
+  | I -> print_char 'I'; print_newline ();
+  | K -> print_char 'K'; print_newline ();
+  | F -> print_char 'F'; print_newline ();
+  | B -> print_char 'B'; print_newline ();
+  | DT -> print_string "DT"; print_newline ();
+  | ST -> print_string "ST"; print_newline ();
+  | SAdr a -> print_string a; print_newline ();
+  | _ -> ()
 
 let pp_ins i =
   match i with
@@ -57,7 +71,40 @@ let pp_ins i =
     print_endline ("ADD V" ^ (string_of_int n) ^ " " ^ (string_of_int m))
   | JP (Adr n) -> print_endline ("JP " ^ (string_of_int n))
   | JP (SAdr s) -> print_endline ("JP " ^ s)
-  | _ -> print_endline "INS ..."
+  | ADD (a, b) -> print_endline "ADD"
+  | LD a -> print_endline "LD a"
+  | LD2 (Reg a, Reg b) -> print_endline "LD V V"
+  | LD2 (Reg a, DT) -> print_endline "LD V DT"
+  | LD2 (DT, Reg a) -> print_endline "LD DT V"
+  | LD2 (ST, Reg a) -> print_endline "LD ST V"
+  | LD2 (F, Reg a) -> print_endline "LD F V"
+  | LD2 (B, Reg a) -> print_endline "LD B V"
+  | LD2 (Reg a, Cst k) -> print_endline "LD V cst"
+  | LD2 (Reg a, K) -> print_endline "LD V K"
+  | LD2 (I, Adr a) -> print_endline "LD V cst"
+  | LD2 (a, b) -> print_endline "Ukn LD"; pp_arg a; pp_arg b
+  | XOR (a, b) -> print_endline "XOR"
+  | OR (a, b) -> print_endline "OR"
+  | AND (a, b) -> print_endline "AND"
+  | SHR (a, b) -> print_endline "SHR"
+  | SHL (a, b) -> print_endline "SHL"
+  | SKP a -> print_endline "SKP"
+  | SKNP a -> print_endline "SKNP"
+  | JP a -> print_endline "JP"
+  | CLS -> print_endline "CLS"
+  | RET -> print_endline "RET"
+  | SUB (a, b) -> print_endline "SUB"
+  | SUBN (a, b) -> print_endline "SUBN"
+  | DRW (a, b, c) -> print_endline "DRW"
+  | CALL a -> print_endline "CALL"
+  | SE (a, b) -> print_endline "SE"
+  | SNE (a, b) -> print_endline "SNE"
+  | DW a -> print_endline "DW"
+  | LBL a -> print_endline "lbl"
+  | END -> print_endline "END"
+  | RND (a, b) -> print_endline "RND"
+  | JP2 (_, _) -> print_endline "JP2"
+  (* | _ -> print_endline "INS ..." *)
 
 
 let get_reg r =
@@ -127,10 +174,13 @@ let process_ld pks =
   | (Lsym "DT", Lsep, Lsym n) when is_reg n -> LD2 (DT, Reg (get_reg n))
   | (Lsym "ST", Lsep, Lsym n) when is_reg n -> LD2 (ST, Reg (get_reg n))
   | (Lsym n, Lsep, Lsym "ST") when is_reg n -> LD2 (Reg (get_reg n), ST)
-  | (Lsym "I", Lsep, Lsym m)    -> LD (SAdr m)
-  | (Lsym n, Lsep, Lsym "K") when is_reg n  -> LD2 (SAdr n, K)
+  | (Lsym "I", Lsep, Lsym n) when is_reg n  -> LD2 (I, Reg (get_reg n))
+  | (Lsym n, Lsep, Lsym "I") when is_reg n  -> LD2 (Reg (get_reg n), I)
+  | (Lsym "I", Lsep, Lsym m)    -> LD2 (I, SAdr m)
+  | (Lsym n, Lsep, Lsym "K") when is_reg n  -> LD2 (Reg (get_reg n), K)
   | (Lsym "B", Lsep, Lsym n) when is_reg n  -> LD2 (B, Reg (get_reg n))
   | (Lsym "F", Lsep, Lsym n) when is_reg n  -> LD2 (F, Reg (get_reg n))
+  | (Lsym n, Lsep, Lsym m) when is_reg n && is_reg m -> LD2 (Reg (get_reg n), Reg (get_reg m))
   | (Lsym n, Lsep, Lsym m) when is_reg n    -> LD2 (Reg (get_reg n), SAdr m)
   | (Lsym n, Lsep, Lnum m) when is_reg n    -> LD2 (Reg (get_reg n), Cst m)
   | (a, _, b) -> pp_lexem a; pp_lexem b; failwith "Incorrect use of LD"
@@ -201,6 +251,9 @@ let wb ins oc =
   | JP (Adr a) ->
     output_char oc (char_of_int (0x10 + (hl a)));
     output_char oc (char_of_int (hr a))
+  | JP2 (Reg x, Adr a) ->
+    output_char oc (char_of_int (0xB0 + (hl a)));
+    output_char oc (char_of_int (hr a))
   | CLS ->
     output_char oc (char_of_int 0x00);
     output_char oc (char_of_int 0xE0)
@@ -228,7 +281,6 @@ let wb ins oc =
   | LD2 (Reg x, Reg y) ->
     output_char oc (char_of_int (0x80 + x));
     output_char oc (char_of_int (y*16))
-  | LD a -> ()
   | OR (Reg x, Reg y) ->
     output_char oc (char_of_int (0x80 + x));
     output_char oc (char_of_int (y*16 + 1))
@@ -255,9 +307,6 @@ let wb ins oc =
     output_char oc (char_of_int (y*16))
   | LD2 (I, Adr n) ->
     output_char oc (char_of_int (0xA0 + hl (n)));
-    output_char oc (char_of_int (hr n))
-  | LD2 (Reg 0, Adr n) ->
-    output_char oc (char_of_int (0xB0 + hl (n)));
     output_char oc (char_of_int (hr n))
   | RND (Reg x, Cst k) ->
     output_char oc (char_of_int (0xC0 + x));
@@ -302,7 +351,7 @@ let wb ins oc =
     output_char oc (char_of_int (n lsr 8));
     output_char oc (char_of_int (n land 0x00FF))
   | END -> ()
-  | _ -> ()
+  | _ as i -> print_endline "BIN Not found"; pp_ins i
 
 
 let set_adresses l =
@@ -319,6 +368,8 @@ let rec replace_adresses l a =
     match x with
     | LD2 (I, (SAdr s)) -> LD2 (I, (Adr (List.assoc s a)))
     | LD (SAdr s) -> LD (Adr (List.assoc s a))
+    | CALL (SAdr s) -> CALL (Adr (List.assoc s a))
+    | JP2 (Reg x, SAdr s) -> JP2 (Reg x, Adr (List.assoc s a))
     | JP (SAdr s) -> JP (Adr (List.assoc s a))
     | _ as ins -> ins
   in
@@ -340,6 +391,6 @@ let _ =
   let l2 = set_adresses l in
   let l3 = replace_adresses l l2 in
   List.iter (fun (a, b) -> print_string (a^" : "); print_int b; print_newline ()) l2;
-  List.iter pp_ins l3;
+  (* List.iter pp_ins l3; *)
   List.iter (fun x -> wb x oc) l3;
   close_out oc
